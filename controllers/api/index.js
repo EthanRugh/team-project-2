fc819c418369ccf6b2c6a292d00e5d0c6daa5e1b
Christopher <ctijerina@live.com>
2022-02-10 20:16:24 -0600

    added recipe routes and tested in insomnia, all working.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const userRoutes = require("./user-routes.js");
 const commentRoutes = require("./comment-routes");
+const recipeRoutes = require("./recipe-routes");
 
 router.use("/users", userRoutes);
 router.use("/comments", commentRoutes);
+router.use("/recipes", recipeRoutes);
 
 module.exports = router;
---
+++ controllers/api/recipe-routes.js
@@ -0,0 +1,159 @@
+const router = require("express").Router();
+const sequelize = require("../../config/connection");
+const { Recipe, User, Comment, Vote } = require("../../model");
+const withAuth = require("../../utils/auth");
+
+// get all users
+router.get("/", (req, res) => {
+	console.log("======================");
+	Recipe.findAll({
+		attributes: [
+			"id",
+			"title",
+			"recipe_text",
+			[
+				sequelize.literal(
+					"(SELECT COUNT(*) FROM vote WHERE recipe.id = vote.recipe_id)"
+				),
+				"vote_count",
+			],
+		],
+		include: [
+			{
+				model: Comment,
+				attributes: ["id", "comment_text", "recipe_id", "user_id"],
+				include: {
+					model: User,
+					attributes: ["username"],
+				},
+			},
+			{
+				model: User,
+				attributes: ["username"],
+			},
+		],
+	})
+		.then((dbPostData) => res.json(dbPostData))
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
+
+router.get("/:id", (req, res) => {
+	Recipe.findOne({
+		where: {
+			id: req.params.id,
+		},
+		attributes: [
+			"id",
+			"title",
+			"recipe_text",
+			[
+				sequelize.literal(
+					"(SELECT COUNT(*) FROM vote WHERE recipe.id = vote.recipe_id)"
+				),
+				"vote_count",
+			],
+		],
+		include: [
+			{
+				model: Comment,
+				attributes: ["id", "comment_text", "recipe_id", "user_id"],
+				include: {
+					model: User,
+					attributes: ["username"],
+				},
+			},
+			{
+				model: User,
+				attributes: ["username"],
+			},
+		],
+	})
+		.then((dbPostData) => {
+			if (!dbPostData) {
+				res.status(404).json({ message: "No recipe found with this id" });
+				return;
+			}
+			res.json(dbPostData);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
+
+router.post("/", (req, res) => {
+	// expects {title: 'recipe title', recipe_text: 'insert recipe here', user_id: 1}
+	console.log(req.params);
+	Recipe.create({
+		title: req.body.title,
+		recipe_text: req.body.recipe_text,
+		user_id: req.body.user_id,
+	})
+		.then((dbPostData) => res.json(dbPostData))
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
+
+router.put("/upvote", (req, res) => {
+	// custom static method created in models/Recipe.js
+	Recipe.upvote(
+		{ ...req.body, user_id: req.session.user_id },
+		{ Vote, Comment, User }
+	)
+		.then((updatedVoteData) => res.json(updatedVoteData))
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
+
+router.put("/:id", (req, res) => {
+	Recipe.update(
+		{
+			title: req.body.title,
+		},
+		{
+			where: {
+				id: req.params.id,
+			},
+		}
+	)
+		.then((dbPostData) => {
+			if (!dbPostData) {
+				res.status(404).json({ message: "No recipe found with this id" });
+				return;
+			}
+			res.json(dbPostData);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
+
+router.delete("/:id", (req, res) => {
+	console.log("id", req.params.id);
+	Recipe.destroy({
+		where: {
+			id: req.params.id,
+		},
+	})
+		.then((dbPostData) => {
+			if (!dbPostData) {
+				res.status(404).json({ message: "No recipe found with this id" });
+				return;
+			}
+			res.json(dbPostData);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(500).json(err);
+		});
+});
+
+module.exports = router;
