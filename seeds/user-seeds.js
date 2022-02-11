dae9c47851856ac703c1d202309ce50c2f3865d5
ethanrugh <erock1114@yahoo.com>
2022-02-10 20:31:12 -0600

    add seed data

---
+++ seeds/user-seeds.js
@@ -0,0 +1,34 @@
+const sequelize = require('../config/connection');
+const { User, Post } = require('../models');
+
+const userdata = [
+    {
+        username: 'jordan',
+        email: 'test@email.com',
+        password: 'password123'
+    },
+    {
+        username: 'chris',
+        email: 'chris@email.com',
+        password: 'password123'
+    },
+    {
+        username: 'ethan',
+        email: 'ethan@email.com',
+        password: 'password123'
+    },
+    {
+        username: 'john',
+        email: 'john@email.com',
+        password: 'password123'
+    },
+    {
+        username: 'steve',
+        email: 'steve@email.com',
+        password: 'password123'
+    }
+];
+
+const seedUsers = () => User.bulkCreate(userdata, {individualHooks: true});
+
+module.exports = seedUsers;
