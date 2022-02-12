3f34bf34b766b41a6f7025c9cb9e0fb836ad40e1
Christopher <ctijerina@live.com>
2022-02-12 13:31:08 -0600

    fixed sesssion and allows for signup properly

--- server.js
+++ server.js
@@ -4,17 +4,17 @@ const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-	secret: 'Super secret secret',
+	secret: "Super secret secret",
 	cookie: {},
 	resave: false,
 	saveUninitialized: true,
 	store: new SequelizeStore({
-		db: sequelize
-	})
+		db: sequelize,
+	}),
 };
 
 const app = express();
@@ -27,9 +27,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // turn on routes routes had to go to the bottom to make it work
 app.use(express.static(path.join(__dirname, "public")));
-app.use(routes);
-app.use(express.static(path.join(__dirname, "public")));
 app.use(session(sess));
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
 	app.listen(PORT, () => console.log("Now listening!"));
