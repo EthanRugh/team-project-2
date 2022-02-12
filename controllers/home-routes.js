9fe20c2e399b7607a4bd915e3d562447875f2256
chellesjord <m.kayejordan@gmail.com>
2022-02-12 12:25:00 -0600

    login homepage and handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,35 +3,40 @@ const sequelize = require('../config/connection');
 const { Recipe, User, Comment } = require('../model');
 
 router.get('/', (req, res) => {
-    Recipe.findAll({
-      attributes: [
-        'id',
-        'title',
-        'recipe_text',
-      ],
-      include: [
-        {
-          model: Comment,
-          attributes: ['id', 'comment_text', 'user_id'],
-          include: {
-            model: User,
-            attributes: ['username']
-          }
-        },
-        {
+  Recipe.findAll({
+    attributes: [
+      'id',
+      'title',
+      'recipe_text',
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'user_id'],
+        include: {
           model: User,
           attributes: ['username']
         }
-      ]
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbRecipeData => {
+      const recipes = dbRecipeData.map(recipe => recipe.get({ plain: true }))
+      // pass a single recipe object into the homepage template
+      res.render('homepage', { recipes });
     })
-      .then(dbRecipeData => {
-        // pass a single recipe object into the homepage template
-        res.render('homepage', { Recipe });
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-      });
-  });
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/login', (req, res) => {
+  res.render('login');
+});
 
 module.exports = router;
