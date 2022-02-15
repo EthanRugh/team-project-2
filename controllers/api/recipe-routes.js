25196e353a47ffc6fdd4d4f4746b87530282be94
Christopher <ctijerina@live.com>
2022-02-14 23:34:11 -0600

    changed up the handlebars and css to match the styling we talked about before in class, still need to connect handlebars to routes properly and add in creating recipes with images

--- controllers/api/recipe-routes.js
+++ controllers/api/recipe-routes.js
@@ -76,7 +76,7 @@ router.get("/:id", (req, res) => {
 				res.status(404).json({ message: "No recipe found with this id" });
 				return;
 			}
-			res.json(dbRecipeData);
+			res.render("recipe", { dbRecipeData });
 		})
 		.catch((err) => {
 			console.log(err);
