94cfd41ea8505bab44d00d4120b78f000b9b32a7
Christopher <ctijerina@live.com>
2022-02-15 20:57:28 -0600

    dashboard works and home route has been fixed

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -39,7 +39,7 @@ router.get("/login", (req, res) => {
 	res.render("login");
 });
 
-router.get("/:id", (req, res) => {
+router.get("/recipes/:id", (req, res) => {
 	Recipe.findOne({
 		where: {
 			id: req.params.id,
