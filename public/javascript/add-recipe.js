c111d3ca2831891c404ad04abf1d622ed0ea73a7
Christopher <ctijerina@live.com>
2022-02-16 21:02:32 -0600

    added in edit recipe handlebars and some of the file upload functions

--- public/javascript/add-recipe.js
+++ public/javascript/add-recipe.js
@@ -1,28 +1,61 @@
-async function newFormHandler(event) {
-	event.preventDefault();
+window.addEventListener("DOMContentLoaded", function () {
+	const apikey = "AWnIKds6OSDycXnY6OkAGz";
+	const client = filestack.init(apikey);
 
-	const title = document.querySelector('input[name="recipe-title"]').value;
+	const onProgress = (evt) => {
+		document.getElementById("progress").innerHTML = `${evt.totalPercent}%`;
+	};
 
-	const recipe_text = document.querySelector('input[name="recipe-text"]').value;
+	document.querySelector("input").addEventListener("change", (event) => {
+		const files = event.target.files[0];
+		const token = {};
+		const cancel = document.getElementById("cancel");
+		const pause = document.getElementById("pause");
+		const resume = document.getElementById("resume");
 
-	const response = await fetch(`/api/recipes`, {
-		method: "POST",
-		body: JSON.stringify({
-			title,
-			recipe_text,
-		}),
-		headers: {
-			"Content-Type": "application/json",
-		},
+		[cancel, resume, pause].forEach((btn) => {
+			const id = btn.id;
+			btn.addEventListener("click", () => {
+				token[id]();
+			});
+		});
+
+		client
+			.upload(files, { onProgress }, {}, token)
+			.then((res) => {
+				console.log("success: ", res);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
 	});
+});
 
-	if (response.ok) {
-		document.location.replace("/");
-	} else {
-		alert(response.statusText);
-	}
-}
+// async function newFormHandler(event) {
+// 	event.preventDefault();
 
-document
-	.querySelector(".new-recipe-form")
-	.addEventListener("submit", newFormHandler);
+// 	const title = document.querySelector('input[name="recipe-title"]').value;
+
+// 	const recipe_text = document.querySelector('input[name="recipe-text"]').value;
+
+// 	const response = await fetch(`/api/recipes`, {
+// 		method: "POST",
+// 		body: JSON.stringify({
+// 			title,
+// 			recipe_text,
+// 		}),
+// 		headers: {
+// 			"Content-Type": "application/json",
+// 		},
+// 	});
+
+// 	if (response.ok) {
+// 		document.location.replace("/");
+// 	} else {
+// 		alert(response.statusText);
+// 	}
+// }
+
+// document
+// 	.querySelector(".new-recipe-form")
+// 	.addEventListener("submit", newFormHandler);
--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require("express");
 const routes = require("./controllers");
 const sequelize = require("./config/connection");
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
+const helpers = require("./utils/helpers");
+const hbs = exphbs.create({ helpers });
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
@@ -25,9 +26,9 @@ app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// turn on routes routes had to go to the bottom to make it work
 app.use(express.static(path.join(__dirname, "public")));
 app.use(session(sess));
+// turn on routes routes had to go to the bottom to make it work
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
