fead1e15e9ca561f40b1b575469868bd164b150b
Christopher <ctijerina@live.com>
2022-02-17 15:31:56 -0600

    styling on edit page updated, still some issues, but a functional MVP

--- public/javascript/delete-recipe.js
+++ public/javascript/delete-recipe.js
@@ -9,7 +9,7 @@ async function deleteFormHandler(event) {
 	});
 
 	if (response.ok) {
-		document.location.replace("/");
+		document.location.replace("/dashboard");
 	} else {
 		alert(response.statusText);
 	}
